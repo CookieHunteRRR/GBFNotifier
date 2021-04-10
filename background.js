b7a770203afca20c4ad2cc1ee24a5317c0c2bf6d
CookieHunteRRR <cookievip2014@gmail.com>
2021-04-10 22:08:44 +0700

    Add files via upload
    оно работает

---
+++ background.js
@@ -0,0 +1,23 @@
+var currentRaidId = undefined;
+
+chrome.tabs.onActivated.addListener(function(activeInfo){
+	chrome.tabs.get(activeInfo.tabId, function(tab){});
+});
+
+chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
+	if (change.url != undefined)
+	{
+		if (tab.url.indexOf("game.granbluefantasy.jp") >= 0)
+		{
+			if (tab.url.substring(31).indexOf("result_multi") >= 0)
+			{
+				chrome.notifications.create('', {
+							title: "Raid ended",
+							message: "ale blyat",
+							iconUrl: "/bell.png",
+							type: "basic"});
+			}
+		}
+	}
+	
+});
